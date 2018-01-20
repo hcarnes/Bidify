2adaa7733b76c05433e0b257c72dfa451230f4f6
Hannah Carnes <hecarnes@gmail.com>
2018-01-20 17:35:35 -0500

    GET \login route working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,9 @@
 class UsersController < ApplicationController
-
   get '/signup' do
-    erb :'users/new'
+    erb :'users/create_user'
+  end
+
+  get '/login' do
+    erb :'users/login'
   end
 end
