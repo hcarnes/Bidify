63d5e9a95bb7c48ccfa7e5414c5ae756c91518d2
Hannah Carnes <hecarnes@gmail.com>
2018-01-20 17:26:12 -0500

    add configure method to ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,17 @@
 require './config/environment'
-class ApplicationController < Sinatra::Base
-  set :views, Proc.new { File.join(root, "../views") }
 
+class ApplicationController < Sinatra::Base
+  helpers ApplicationHelpers
+
+  configure do
+    set :public_folder, 'public'
+    enable :sessions
+    set :session_secret, "password_security"
+    set :show_exceptions, false unless ENV['SINATRA_ENV'] == 'development'
+    set :views, Proc.new { File.join(root, "../views") }
+  end
+ 
   get '/' do
     erb :index
-  end
-
-  get '/signup' do
-    erb :'users/new'
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,4 @@
 class UsersController < ApplicationController
-  set :views, Proc.new { File.join(root, "../views/users") }
 
   get '/signup' do
     erb :'users/new'
