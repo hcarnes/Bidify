9c35f6d82eeb9812e7b3ebe0e7b1bcce1da4986d
Hannah Carnes <hecarnes@gmail.com>
2018-01-20 22:46:10 -0500

    ensure starting bid is respected

--- app/models/bid.rb
+++ app/models/bid.rb
@@ -5,10 +5,17 @@ class Bid < ActiveRecord::Base
   validates_presence_of :user_id
   validates_presence_of :listing_id
   validate :no_self_bidding
+  validate :amount_higher_than_starting_bid
 
   def no_self_bidding
     if self.user == self.listing.user
       errors.add(:user_id, "can't bid on your own item :)")
     end    
+  end
+
+  def amount_higher_than_starting_bid
+    if self.listing.starting_bid > self.amount
+      errors.add(:amount, "bid must be higher than starting amount")
+    end
   end
 end
