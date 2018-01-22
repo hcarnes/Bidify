0f27f146dfc8d7383aa0d72832f7926bdf54bfca
Hannah Carnes <hecarnes@gmail.com>
2018-01-21 19:37:49 -0500

    removed slug methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,12 +5,4 @@ class User < ActiveRecord::Base
 
   validates_presence_of :username, :email
   validates_uniqueness_of :username, :email
-
-  def slug
-    self.username.downcase.gsub(" ", "-")
-  end
-
-  def self.find_by_slug(slug)
-    self.where("LOWER(username) = ?", slug.gsub("-", " ")).first
-  end
 end
